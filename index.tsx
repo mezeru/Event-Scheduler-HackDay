17b0929c1899e325041d13d6b9c7bfa4dc4a244b
Yash <jermpro0@gmail.com>
2023-07-17 13:21:59 -0400

    Client commit

---
+++ index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
